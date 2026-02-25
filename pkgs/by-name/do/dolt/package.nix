853c5f19b9bf8e81da2e13ec44058d676abb861e
Edward Langley <el-github@elangley.org>
2026-02-25 00:45:24 -0800

    fix(dolt): add dependencies to upgrade dolt

--- pkgs/by-name/do/dolt/package.nix
+++ pkgs/by-name/do/dolt/package.nix
@@ -2,24 +2,28 @@
   fetchFromGitHub,
   lib,
   buildGoModule,
+  icu,
+  pkg-config
 }:
 
 buildGoModule (finalAttrs: {
   pname = "dolt";
-  version = "1.59.10";
+  version = "1.82.4";
 
   src = fetchFromGitHub {
     owner = "dolthub";
     repo = "dolt";
     tag = "v${finalAttrs.version}";
-    hash = "sha256-DfocUOHpPdNeMcL7kVm7ggm2cVgWp/ifvCFyFosxhcs=";
+    hash = "sha256-mavL3y+Kv25hzFlDFXk7W/jeKVKlCBjlc67GkL3Jcwk=";
   };
 
   modRoot = "./go";
   subPackages = [ "cmd/dolt" ];
-  vendorHash = "sha256-yZ+q4KNfIiR2gpk10dpZOMiEN3V/Lk/pzhgaqp7lKag=";
+  vendorHash = "sha256-K1KzsqptZxO5OraWKIXeqKuVSzb6E/Mjy3c5PQ7Rs9k=";
   proxyVendor = true;
   doCheck = false;
+  buildInputs = [icu.dev pkg-config];
+
 
   meta = {
     description = "Relational database with version control and CLI a-la Git";
